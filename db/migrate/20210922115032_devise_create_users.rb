c4964c88bc80a2904aeb1dd14ed13a786347779d
ogguraaaaa <o7o1464o5o2.ogura@gmail.com>
2021-09-23 17:21:46 +0900

    registrationsにemail追加

--- db/migrate/20210922115032_devise_create_users.rb
+++ db/migrate/20210922115032_devise_create_users.rb
@@ -4,7 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       # Database authenticatable
-      ## t.string :email,              null: false, default: ""
+      t.string :email,              null: false, default: "", unique: true
       t.string :encrypted_password, null: false, default: ""
       t.string :name,               null: false
       t.string :number,             null: false
@@ -39,7 +39,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.timestamps null: false
     end
 
-    # add_index :users, :email,                unique: true
+    add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
